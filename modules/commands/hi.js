7d9920b5fabe5bf574079d770b2211e2e5538dfa
Quang <100977268+haoquang2009@users.noreply.github.com>
2022-06-18 16:28:50 +0700

    Add files via upload

---
+++ modules/commands/hi.js
@@ -0,0 +1,42 @@
+module.exports.config = {
+    name: "hi",
+    version: "1.0.1",
+    hasPermssion: 0,
+    credits: "Kanichi",
+    description: "",
+    commandCategory: "không cần dấu lệnh",
+    usages: "",
+    cooldowns: 0,
+    denpendencies: {
+        "fs": "",
+        "request": ""
+    }
+};
+module.exports.onLoad = () => {
+    const fs = require("fs-extra");
+    const request = require("request");
+    const dirMaterial = __dirname + `/noprefix/`;
+    if (!fs.existsSync(dirMaterial + "noprefix")) fs.mkdirSync(dirMaterial, { recursive: true });
+    if (!fs.existsSync(dirMaterial + "hi.gif")) request("https://imgur.com/a/fSt6iE4").pipe(fs.createWriteStream(dirMaterial + "hi.gif"));
+}
+module.exports.handleEvent = async ({ event, api, Currencies,Users, args, utils, global, client }) => {
+    const fs = require("fs");
+    let name = await Users.getNameUser(event.senderID)
+    var msg = {
+                body: `💘Hiii,chào bạn ${name}💕,Chúc bạn một ngày mới tốt lành ❤️`,
+                attachment: fs.createReadStream(__dirname + `/noprefix/hi.gif`)
+            }
+    if (event.body.toLowerCase() == "hi"){
+        return api.sendMessage(msg,event.threadID,event.messageID);}
+    if (event.body.toLowerCase() == "hello"){
+        return api.sendMessage(msg,event.threadID,event.messageID);}
+    if (event.body.toLowerCase() == "chào"){
+        return api.sendMessage(msg,event.threadID,event.messageID);}
+    if (event.body.toLowerCase() == "2"){
+        return api.sendMessage(msg,event.threadID,event.messageID);}
+    if (event.body.toLowerCase() == "lô"){
+        return api.sendMessage(msg,event.threadID,event.messageID);}
+        };
+module.exports.run = async ({ event, api, Currencies, args, utils }) => {
+return api.sendMessage("Dùng sai cách rồi lêu lêu",event.threadID)
+    }
